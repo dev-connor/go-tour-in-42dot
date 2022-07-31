d46321d3afe2ceadc2cd7860b9b43896677d062c
Connor <kor.connor@gmail.com>
2022-07-31 13:24:44 +0900

    5번 문제 풀이

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func add(a, b int) int {
 	return a + b
@@ -10,9 +13,21 @@ func mul(a, b int) int64 {
 	return int64(a) * int64(b)
 }
 
+func div(a, b int) (float64, error) {
+	switch b {
+	case 0:
+		error := errors.New("분모는 0 이 될 수 없습니다")
+		return 0, error
+	default:
+		answer := float64(a) / float64(b)
+		return answer, nil
+	}
+}
+
 func main() {
 	fmt.Println(add(1, 2))
 	fmt.Println(mul(2, 3))
+	fmt.Println(div(7, 3))
 
 	/*
 	   if val, err := div(2, 3); err == nil {
